b41460608e2a62404543c4bae588f97ddcb75487
Mark Merritt <34792721+marksmerritt@users.noreply.github.com>
2024-05-12 08:18:24 -0400

    Config redis (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   get "up" => "rails/health#show", as: :rails_health_check
   root "marketing/pages#index"
 
