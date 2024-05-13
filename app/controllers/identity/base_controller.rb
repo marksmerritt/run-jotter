74b3f3bbc228336c927e91a1d338a8f2cf20a66a
Mark Merritt <markmerritt804@gmail.com>
2024-05-13 12:01:29 -0400

    Style auth pages

--- app/controllers/identity/base_controller.rb
+++ app/controllers/identity/base_controller.rb
@@ -1,3 +1,13 @@
 class Identity::BaseController < ApplicationController
 	layout "identity"
+
+	before_action :redirect_if_signed_in
+
+	private
+
+	def redirect_if_signed_in
+		return unless user_signed_in?
+
+		redirect_to dashboard_path
+	end
 end
