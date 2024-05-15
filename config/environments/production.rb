aef6e062482224bd253a747c0122e38902e1c401
Mark Merritt <34792721+marksmerritt@users.noreply.github.com>
2024-05-15 08:49:38 -0400

    Config email (#5)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,6 +73,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  ActionMailer::Base.smtp_settings = {
+    address: ENV["MAILGUN_SMTP_SERVER"],
+    port: ENV["MAILGUN_SMTP_PORT"],
+    domain: "runjotter-5f1be84be17f.herokuapp.com/",
+    user_name: ENV["MAILGUN_SMTP_LOGIN"],
+    password: ENV["MAILGUN_SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = { host: "runjotter-5f1be84be17f.herokuapp.com" }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
